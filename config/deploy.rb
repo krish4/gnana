9b08db9405d6a43a80dc3dc64a77589f6fc9c864
krish <krishkongara@gmail.com>
2017-01-31 23:50:55 +0530

    updated deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,8 @@ set :repo_url, "https://github.com/krish4/gnana.git"
 
 set :deploy_to, '/home/deploy/gnana'
 
-#append :linked_files, "config/database.yml", "config/secrets.yml"
-#ppend :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
