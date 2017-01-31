addf63bd0bbf05c021e416584681fa3baad2b7dc
krish <krishkongara@gmail.com>
2017-02-01 00:21:50 +0530

    update prodution.rb file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '138.197.89.44', user: 'root', roles: %w{app db web}
+server '138.197.89.44', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
